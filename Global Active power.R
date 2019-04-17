ed6c0398633e0e7ea21dd408122a2aaf97a1cdd7
charlesgto <okobedoma@gmail.com>
2019-04-17 16:54:02 +0100

    Add files via upload

---
+++ Global Active power.R
@@ -0,0 +1,13 @@
+setwd("~/R_PROGRAME/exdata_data_household_power_consumption")
+DataHPC <- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?", nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+data1 <- subset(dataHPC, Date %in% c("1/2/2007","2/2/2007"))
+Error in subset(dataHPC, Date %in% c("1/2/2007", "2/2/2007")) : 
+  object 'dataHPC' not found
+data1 <- subset(DataHPC, Date %in% c("1/2/2007","2/2/2007"))
+data1$Date <- as.Date(data1$Date, format="%d/%m/%Y")
+atetime <- paste(as.Date(data1$Date), data1$Time)
+data1$Datetime <- as.POSIXct(datetime)
+with(data1, {
+  plot(Global_active_power~Datetime, type="l",
+       ylab="Global Active Power (kilowatts)", xlab="")
+})
